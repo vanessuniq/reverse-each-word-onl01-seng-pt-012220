888bb1c34b71c2de641ab8658f3f85c668d26d4e
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-31 09:05:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word string
-  string_to_array = string.split
-  string.split.collect.join(' ') do |word|
+  string_to_array = string.split(" ")
+  string_to_array.collect.join(' ') do |word|
     word.reverse
   end 
 end
